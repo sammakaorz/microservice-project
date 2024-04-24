e7d532bb527bcbc3c6058556251ae7bc73e965af
sammakaorz <sammakaorz@hotmail.com>
2024-04-24 19:29:22 -0400

    created versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      owner = "sammakaorz"
+    }
+  }
+}
